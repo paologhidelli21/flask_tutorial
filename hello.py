3ffd3a93fbcaa737834b2704f7dda741295972ae
paologhidelli21 <paologhidelli21@gmail.com>
2025-01-13 18:08:00 +0000

    variabili

--- hello.py
+++ hello.py
@@ -11,8 +11,28 @@ def hello_world():
 def index():
     return "<p>Index Page</p>"
 
-@app.route("/<name>")
+
+@app.route("/projects/")
+def projects():
+    return "<p>The project page</p>"
+
+
+@app.route("/name/<name>")
 def hello(name):
     return f"Hello, {escape(name)}!"
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    #show the user profile for that user
+    return f'User {escape(username)}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    #show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    #show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
 
