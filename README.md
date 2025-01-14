f1eb994ffbfbec1a26235c821492db4d84fc46dc
paologhidelli21 <paologhidelli21@gmail.com>
2025-01-14 14:01:50 +0000

    template

--- README.md
+++ README.md
@@ -2,9 +2,18 @@
 applicazione basata sulla documentazione di Flask "Quickstart":
 https://flask.palletsprojects.com/en/stable/quickstart/
 
+template:
+https://jinja.palletsprojects.com/templates/
+https://jinja.palletsprojects.com/en/stable/templates/
+https://flask.palletsprojects.com/en/stable/patterns/templateinheritance/
+
+risorse html e css:
+https://developer.mozilla.org/en-US/docs/Learn_web_development/Getting_started/Your_first_website
+
 
 source venv/bin/activate
 pip freeze > requirements.txt
+flask --app hello run --debug
 
 Start Command
 gunicorn main:app
--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, request, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -11,20 +11,36 @@ def hello_world():
 def index():
     return "<p>Index Page</p>"
 
+#HTTP Methods
+#Flask. By default, a route only answers to GET requests
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return do_the_login()
+    else:
+        return show_the_login_form()
+    
+#Rendering Templates
+#https://jinja.palletsprojects.com/templates/
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', person=name)
 
 @app.route("/projects/")
 def projects():
     return "<p>The project page</p>"
 
-
+'''
 @app.route("/name/<name>")
 def hello(name):
     return f"Hello, {escape(name)}!"
-
+'''
+    
 @app.route('/user/<username>')
-def show_user_profile(username):
+def profile(username):
     #show the user profile for that user
-    return f'User {escape(username)}'
+    return f'{escape(username)}\'s profile'
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
@@ -36,3 +52,10 @@ def show_subpath(subpath):
     #show the subpath after /path/
     return f'Subpath {escape(subpath)}'
 
+#stampa sul terminale i valori restituiti dalla funzione di reversin url
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('login'))
+    print(url_for('login', next='/'))
+    print(url_for('profile', username='John Doe'))
+    print(url_for('static', filename='css/templates.css'))
